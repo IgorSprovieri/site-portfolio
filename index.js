d7d2d6ae139eb1ed8f2e7724ae453742bb18a1c7
IgorSprovieri <igsp1@hotmail.com>
2022-12-15 09:01:59 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+window.onload(() => {
+  //alert("test");
+});
+
+function scrollPageTo(id) {
+  document.getElementById(id).scrollIntoView();
+}
+
+function openNewTab(url) {
+  window.open(url);
+}
