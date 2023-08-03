2100982ff4216837c0b6d6e94cd70997ee596f72
Igor Sprovieri <igsp1@hotmail.com>
2023-08-03 16:08:05 -0300

    Feat: View all repositories

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
--- src/sections/sectionProjects/index.js
+++ src/sections/sectionProjects/index.js
@@ -1,10 +1,20 @@
-import { GitHubButton, Window } from "@/components";
+import { GitHubButton, ProjectCard, Window } from "@/components";
 import { Projects } from "./projects";
 import styles from "./style.module.css";
 import { useState } from "react";
+import axios from "axios";
+import { useQuery } from "react-query";
 
 export const SectionProjects = () => {
   const [windowOpen, setWindowOpen] = useState(false);
+
+  const { data } = useQuery("repos", async () => {
+    return await axios.get(`https://api.github.com/users/IgorSprovieri/repos`, {
+      headers: {
+        Authorization: `token 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`,
+      },
+    });
+  });
 
   return (
     <>
@@ -12,13 +22,33 @@ export const SectionProjects = () => {
         title="Repositórios GitHub"
         open={windowOpen}
         setOpen={setWindowOpen}
-      ></Window>
+      >
+        <div className={styles["list-repos-container"]}>
+          {data?.data?.map((repo) => {
+            return (
+              <ProjectCard
+                key={repo.id}
+                src={"/blank.png"}
+                alt={`repo ${repo.name}`}
+                title={repo.name}
+                Button={
+                  <GitHubButton onClick={() => window.open(repo.html_url)}>
+                    Ver Repositório
+                  </GitHubButton>
+                }
+              >
+                {repo.description}
+              </ProjectCard>
+            );
+          })}
+        </div>
+      </Window>
       <section className={styles["projects-section"]}>
         <div className={styles["projects-container"]}>
           <Projects></Projects>
         </div>
         <GitHubButton onClick={() => setWindowOpen(true)}>
-          Todos os Projetos
+          Todos os Repositórios
         </GitHubButton>
       </section>
     </>
