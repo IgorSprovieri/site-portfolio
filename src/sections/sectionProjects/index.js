b847e6810dc0263bab5c47663aed12f9c2c18ec8
Igor Sprovieri <igsp1@hotmail.com>
2023-08-03 17:03:17 -0300

    Feat: Sort repositories

--- src/sections/sectionProjects/index.js
+++ src/sections/sectionProjects/index.js
@@ -7,24 +7,14 @@ import { useQuery } from "react-query";
 
 export const SectionProjects = () => {
   const [windowOpen, setWindowOpen] = useState(false);
-
-  const { data } = useQuery(
-    "repos",
-    async () => {
-      return await axios.get(
-        `https://api.github.com/users/IgorSprovieri/repos`,
-        {
-          headers: {
-            Authorization: `token ${process.env.NEXT_PUBLIC_TOKEN_GITHUB}`,
-          },
-        }
-      );
-    },
-    {
-      onError: () => console.log(process.env.NEXT_PUBLIC_TOKEN_GITHUB),
-      onSuccess: () => console.log(process.env.NEXT_PUBLIC_TOKEN_GITHUB),
-    }
-  );
+  const [repos, setRepos] = useState([]);
+  const { data } = useQuery("repos", async () => {
+    return await axios.get(`https://api.github.com/users/IgorSprovieri/repos`, {
+      headers: {
+        Authorization: `token ${process.env.NEXT_PUBLIC_TOKEN_GITHUB}`,
+      },
+    });
+  });
 
   return (
     <>
@@ -34,23 +24,25 @@ export const SectionProjects = () => {
         setOpen={setWindowOpen}
       >
         <div className={styles["list-repos-container"]}>
-          {data?.data?.map((repo) => {
-            return (
-              <ProjectCard
-                key={repo.id}
-                src={"/blank.png"}
-                alt={`repo ${repo.name}`}
-                title={repo.name}
-                Button={
-                  <GitHubButton onClick={() => window.open(repo.html_url)}>
-                    Ver Repositório
-                  </GitHubButton>
-                }
-              >
-                {repo.description}
-              </ProjectCard>
-            );
-          })}
+          {data?.data
+            ?.sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
+            ?.map((repo) => {
+              return (
+                <ProjectCard
+                  key={repo.id}
+                  src={"/blank.png"}
+                  alt={`repo ${repo.name}`}
+                  title={repo.name}
+                  Button={
+                    <GitHubButton onClick={() => window.open(repo.html_url)}>
+                      Ver Repositório
+                    </GitHubButton>
+                  }
+                >
+                  {repo.description}
+                </ProjectCard>
+              );
+            })}
         </div>
       </Window>
       <section className={styles["projects-section"]}>
