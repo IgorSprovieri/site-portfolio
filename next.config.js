812d0d606d888f193332b23d1ede49c709e0fe37
Igor Sprovieri <igsp1@hotmail.com>
2023-08-08 11:41:12 -0300

    Fix: React Markdown

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["github-readme-stats.vercel.app"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
